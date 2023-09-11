965ca8767f3db5f4d29de6b44a7e0db948a9472c
rollingsxshi <rollingsxshi@gmail.com>
2023-09-11 22:43:42 +0800

    page metadata

--- app/(dashboard)/tickets/[id]/page.tsx
+++ app/(dashboard)/tickets/[id]/page.tsx
@@ -7,6 +7,16 @@ interface Props {
 
 export const dynamicParams = true
 
+export async function generateMetadata({ params }: Props) {
+  const id = params.id
+  const res = await fetch(`http://localhost:4000/tickets/${id}`)
+  const ticket = await res.json()
+
+  return {
+    title: `Dojo Helpdesk | ${ticket.title}`,
+  }
+}
+
 export async function generateStaticParams() {
   const res = await fetch('http://localhost:4000/tickets')
   const tickets = await res.json()
--- app/(dashboard)/tickets/page.tsx
+++ app/(dashboard)/tickets/page.tsx
@@ -1,6 +1,12 @@
 import { Suspense } from "react"
+import type { Metadata } from 'next'
+
 import TicketList from "./TicketList"
 import Loading from "../loading"
+
+export const metadata: Metadata = {
+  title: 'Dojo Helpdesk | Tickets',
+}
 
 const Tickets = () => {
   return (
