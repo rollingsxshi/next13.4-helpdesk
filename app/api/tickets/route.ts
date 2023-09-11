93b45fc448665b3ec5be3ca3a248abcefde5e1c8
rollingsxshi <rollingsxshi@gmail.com>
2023-09-11 23:19:30 +0800

    handling POST requests

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,13 +1,28 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
 // next GET route handlers are static by default
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function GET() {
-  const res = await fetch('http://localhost:4000/tickets')
-  const tickets = await res.json()
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
 
   return NextResponse.json(tickets, {
     status: 200,
+  });
+}
+
+export async function POST(req: Request) {
+  const ticket = await req.json();
+
+  const res = await fetch("http://localhost:4000/tickets", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(ticket),
+  });
+
+  const newTicket = await res.json()
+  return NextResponse.json(newTicket, {
+    status: 201,
   })
 }
