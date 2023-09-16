7354dd2182423450c81bb26258c106f60b7108d4
rollingsxshi <rollingsxshi@gmail.com>
2023-09-16 19:06:42 +0800

    making auth forms

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+
+import AuthForm from "../AuthForm"
+
 const Login = () => {
+  const handleSubmit = async (e: React.SyntheticEvent, email: string, password: string) => {
+    e.preventDefault()
+
+    console.log('user login', email, password);
+  }
+
   return (
     <main>
       <h2 className="text-center">Log in</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 const Signup = () => {
+  const handleSubmit = async (e: React.SyntheticEvent, email: string, password: string) => {
+    e.preventDefault()
+
+    console.log('user signup', email, password);
+  }
+
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
