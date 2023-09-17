52c4d4000e9a5fd12c8abf37f0a16c0d2c13b485
rollingsxshi <rollingsxshi@gmail.com>
2023-09-17 13:12:36 +0800

    get current user

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,20 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from 'next/headers'
+
+// components
 import Navbar from "../components/Navbar"
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
   children,
 }: {
   children: React.ReactNode
 }) => {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
   return (
     <>
-      <Navbar />
+      <Navbar user={data.session?.user} />
       {children}
     </>
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "./dojo-logo.png";
 
-const Navbar = () => {
+interface User {
+  email: string
+}
+
+interface Props {
+  user: User
+}
+
+const Navbar = ({ user }: Props) => {
   return (
     <nav>
       <Image
@@ -15,6 +23,7 @@ const Navbar = () => {
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
+      {user && <span>Hello, { user.email }</span>}
     </nav>
   );
 };
